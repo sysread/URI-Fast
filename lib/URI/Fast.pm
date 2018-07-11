56895bc47980e44bbff6443e41b73ca6e2454087
Jeff Ober <sysread@fastmail.fm>
2018-07-11 14:35:24 +0000

    Fix typo in docs

--- lib/URI/Fast.pm
+++ lib/URI/Fast.pm
@@ -265,9 +265,9 @@ its value.
 
 Note that because URIs are parsed into a fixed size struct, any method
 (including the constructor) attempting to set a value larger than the max
-allowable size cause the value to be truncated before croaking. If this error
+allowable size causes the value to be truncated before croaking. If this error
 is caught in an eval, the URI will remain nominally usable in its updated,
-truncated state.
+albeit truncated, state.
 
 =head2 scheme
 
