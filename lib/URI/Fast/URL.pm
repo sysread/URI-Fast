5a7faeff0127a79e811b60d6396ea689340b9049
Jeff Ober <jober@ziprecruiter.com>
2021-07-23 09:32:14 -0400

    URLs should only have the scheme replaced if they begin with "//" after cleaning

--- lib/URI/Fast/URL.pm
+++ lib/URI/Fast/URL.pm
@@ -11,7 +11,6 @@ use Encode qw(encode);
 sub uri {
   my ($uri_string, %param) = @_;
   my $encoding = $param{encoding} || 'UTF-8';
-  my $scheme   = URI::Fast::uri( $param{scheme} )->scheme;
 
   # First, interpret the URI string as the encoding from the document from
   # which it presuambly came.
@@ -19,15 +18,17 @@ sub uri {
 
   # Next, remove characters specified by the URL standard
   $uri_string =~ s|[\t\r\n]||g; # strip tabs, line feeds, and carriage returns
-  $uri_string =~ s|\\|/|g;      # convert backslashes to forward slashes
+  $uri_string =~ tr|\\|/|;      # convert backslashes to forward slashes
 
   # Then, parse the URI string into a URI::Fast object
   my $uri = uri $uri_string;
 
-  # Replace the scheme with a reference scheme, presumably from the source
-  # document from which the URI was read.
-  if ($scheme) {
-    $uri->scheme($scheme);
+  # If the URL begins with //, the scheme is replaced with that of a reference
+  # URI, presumably that of the source document from which the URI was read.
+  if ($uri_string =~ /^\/\//) {
+    if (my $scheme = URI::Fast::uri( $param{scheme} )->scheme) {
+      $uri->scheme($scheme);
+    }
   }
 
   return $uri;
