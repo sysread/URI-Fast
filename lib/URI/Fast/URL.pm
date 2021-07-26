02b9558c123851f34afec4d4694abf5e6acabf5a
Jeff Ober <jober@ziprecruiter.com>
2021-07-26 10:54:12 -0400

    Scrub input strings of whitespace chars permitted by html, translate backslashes into forward slashes

--- lib/URI/Fast/URL.pm
+++ lib/URI/Fast/URL.pm
@@ -10,13 +10,8 @@ use Encode qw(encode);
 
 sub uri {
   my ($uri_string, %param) = @_;
-  my $encoding = $param{encoding} || 'UTF-8';
 
-  # First, interpret the URI string as the encoding from the document from
-  # which it presuambly came.
-  $uri_string = encode $encoding, $uri_string, Encode::FB_CROAK;
-
-  # Next, remove characters specified by the URL standard
+  # Remove characters specified by the URL standard
   $uri_string =~ s|[\t\r\n]||g; # strip tabs, line feeds, and carriage returns
   $uri_string =~ tr|\\|/|;      # convert backslashes to forward slashes
 
