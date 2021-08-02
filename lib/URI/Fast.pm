e03d3fa71380dfa9bc9592b6170bb2b9d4be1383
Jeff Ober <jober@ziprecruiter.com>
2021-08-02 14:38:51 -0400

    Oops. Forgot to merge my branch before cutting dev release 0.49_01.

--- lib/URI/Fast.pm
+++ lib/URI/Fast.pm
@@ -1,6 +1,6 @@
 package URI::Fast;
 
-our $XS_VERSION = our $VERSION = '0.49_01';
+our $XS_VERSION = our $VERSION = '0.49_02';
 $VERSION =~ tr/_//;
 
 use utf8;
--- lib/URI/Fast/IRI.pm
+++ lib/URI/Fast/IRI.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 require URI::Fast;
-our $VERSION = '0.49_01';
+our $VERSION = '0.49_02';
 
 our @ISA = qw(URI::Fast);
 
