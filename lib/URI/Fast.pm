f846f7f2e5d100c08ef3805af8a7dac8ca87eaef
Jeff Ober <jober@ziprecruiter.com>
2021-08-31 15:36:33 -0400

    bump version to 0.53

--- lib/URI/Fast.pm
+++ lib/URI/Fast.pm
@@ -1,6 +1,6 @@
 package URI::Fast;
 
-our $XS_VERSION = our $VERSION = '0.52_02';
+our $XS_VERSION = our $VERSION = '0.53';
 $VERSION =~ tr/_//;
 
 use utf8;
--- lib/URI/Fast/IRI.pm
+++ lib/URI/Fast/IRI.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 require URI::Fast;
-our $VERSION = '0.52_02';
+our $VERSION = '0.53';
 
 our @ISA = qw(URI::Fast);
 
